db0a20755db335692c86d3be8e108f8d08ebb1d6
Quentin R <q.roccia@dimsi.fr>
2023-02-24 11:53:12 +0100

    Removed unused property

--- src/ui/entrypoint.sh
+++ src/ui/entrypoint.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
+
+source ../../common/config/ui/env
+
 for file in /usr/share/nginx/html/js/*;
 do
   if [ ! -f $file.tmpl ]; then
     cp $file $file.tmpl
   fi
-  envsubst 'http://localhost:5050 https://sso.dimsi.io/auth backroll master' < $file.tmpl > $file
+  envsubst '$BACKROLL_API_ENDPOINT_URL $BACKROLL_OPENID_ISSUER_URL $BACKROLL_OPENID_CLIENTID $BACKROLL_OPENID_REALM' < $file.tmpl > $file
 
 done
 echo "Starting Nginx"
