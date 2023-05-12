8aa93a275b3a7a5af30a6586304fc6867b45be8b
Christophe Gondouin <14264672+cgoconseils@users.noreply.github.com>
2023-05-12 17:44:26 +0200

    Correction of package build policy regarding environment variables

--- src/ui/entrypoint.sh
+++ src/ui/entrypoint.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
-source ../../common/config/ui/env
-
 for file in /usr/share/nginx/html/js/*;
 do
   if [ ! -f $file.tmpl ]; then
     cp $file $file.tmpl
   fi
   envsubst '$API_ENDPOINT_URL $OPENID_ISSUER $OPENID_CLIENTID $OPENID_REALM' < $file.tmpl > $file
-
+  
 done
+
 echo "Starting Nginx"
 nginx -g 'daemon off;'
