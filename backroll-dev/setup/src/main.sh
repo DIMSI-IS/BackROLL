15d42737268b1a28fdbd58c2a5be0ccc55bf58dc
Ubuntu <ubuntu@ml110g7>
2024-03-25 22:19:51 +0100

    feat: username for Windows

--- backroll-dev/setup/src/main.sh
+++ backroll-dev/setup/src/main.sh
@@ -1,9 +1,12 @@
 echo "Starting BackROLL configuration…"
 
+user="${USERNAME:-${USER:-someone}}"
+echo "Current user: $user"
+
 key_file=backroll-dev/ssh/id
 if ! test -f "$key_file"; then
     echo "Generating ssh key…"
-    ssh-keygen -t ed25519 -N "" -f "$key_file" -C "BackROLL" -q
+    ssh-keygen -t ed25519 -N "" -f "$key_file" -C "BackROLL set up by $user" -q
     echo "…done."
 fi
 
