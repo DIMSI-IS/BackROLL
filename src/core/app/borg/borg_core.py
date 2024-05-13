317ca1a989d83c57cf3d070fc1caf93d99843460
Ubuntu <ubuntu@ml110g7>
2024-05-13 12:06:41 +0200

    fix: disk source extension on backup

--- src/core/app/borg/borg_core.py
+++ src/core/app/borg/borg_core.py
@@ -25,6 +25,7 @@ import calendar
 import time
 import paramiko
 import shutil
+import re
 
 from app import shell
 
@@ -165,23 +166,18 @@ class borg_backup:
   def create_archive(self, disk):
     repository = self.info['borg_repository']
     vm_name = self.virtual_machine['name']
-    disk_source = ((disk['source']).split('.'))[0]
+    disk_source = disk['source']
+    disk_source_path_name = re.sub("\.qcow2$", "", disk_source) # Python >= 3.9: disk_source.removesuffix(".qcow2")
+    disk_source_file_name = disk_source_path_name.split("/")[-1]
     disk_name = disk['device']
 
     print(f'[{vm_name}] Creating borg archive for disk {disk_name}')
     
-    self.info['backup_name'] = f'{disk_name}_{disk_source.split("/")[-1]}_{calendar.timegm(time.gmtime())}'
+    self.info['backup_name'] = f'{disk_name}_{disk_source_file_name}_{calendar.timegm(time.gmtime())}'
 
     if "pool_id" in self.virtual_machine:
       connector = connectors.filter_connector_by_id(pool.filter_pool_by_id(self.virtual_machine["pool_id"]).connector_id)
-      disk_source = f'/mnt/{cs_manage_vm.listStorage(connector, disk)["id"]}/{disk_source}'
-
-    # Detect file name if disk_source has no file extension.
-    [disk_source_directory, disk_source_file] = disk_source.rsplit('/', 1)
-    if '.' not in disk_source_file:
-      available_disk_sources = shell.run(f'find {disk_source_directory} -name "{disk_source_file}.*" | grep -E "/[^.]+\.[^.]+$"')
-      # TODO Pick the right one.
-      disk_source = available_disk_sources[0]
+      disk_source = f'/mnt/{cs_manage_vm.listStorage(connector, disk)["id"]}/{disk_source_path_name}'
 
     shell.run(f"borg create --log-json --progress {repository}{vm_name}::{self.info['backup_name']} {disk_source}")
 
