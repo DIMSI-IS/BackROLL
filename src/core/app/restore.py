f1d7a61f50523b15f1a88fa1f61ef2d879e0e55b
Ubuntu <ubuntu@ml110g7>
2024-05-13 10:47:26 +0200

    refactor: using shell.run instead of os.system

--- src/core/app/restore.py
+++ src/core/app/restore.py
@@ -109,7 +109,7 @@ def restore_task(self, virtual_machine_info, hypervisor, vm_storage_info, backup
       shell.run(f"""borg extract --sparse {borg_repository}{virtual_machine_info['name']}::{backup_name}""")
 
       # Skip directories
-      os.system('mv $(find -type f) ./')
+      shell.run('mv $(find -type f) ./')
 
       # Loop through VM's disks to find filedisk
       for disk in vm_storage_info:
