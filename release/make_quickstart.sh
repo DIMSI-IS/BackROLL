66b638624000d56992555c6152b6d96d4cb38ea0
m.dhellin <m.dhellin@dimsi.fr>
2024-09-03 17:41:32 +0200

    fix exiting on error

--- release/make_quickstart.sh
+++ release/make_quickstart.sh
@@ -1,13 +1,12 @@
 # Makes the quickstart script targeting the latest release.
 
+# Intented to be run with the bash source command.
+# Do not use “set -e” because it will exit the user shell.
 cat <<HEREDOC > quickstart.sh
-set -e
-
-git clone --single-branch --branch main https://github.com/DIMSI-IS/backroll.git BackROLL
-cd BackROLL
-git checkout $(git describe --tags --abbrev=0)
-
-cd compose_project
-source source-me.sh prod
+git clone --single-branch --branch main https://github.com/DIMSI-IS/backroll.git BackROLL && \
+cd BackROLL && \
+git checkout $(git describe --tags --abbrev=0) && \
+cd compose_project && \
+source source-me.sh prod && \
 docker compose \$prod up
 HEREDOC
