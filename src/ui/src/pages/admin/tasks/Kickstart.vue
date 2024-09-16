781f3a6d5b5318f968163464b44a47ad4c1f9582
m.dhellin <m.dhellin@dimsi.fr>
2024-09-16 11:09:13 +0200

    Less duplicated values

--- src/ui/src/pages/admin/tasks/Kickstart.vue
+++ src/ui/src/pages/admin/tasks/Kickstart.vue
@@ -7,183 +7,113 @@
       <va-timeline vertical style="min-width: 200px;">
         <va-timeline-item color="success" :active="isValid(jobSelection) ? true : false">
           <template #before>
-            <va-card
-              stripe
-              stripe-color="info"
-            >
+            <va-card stripe stripe-color="info">
               <va-card-content>
-                <va-select
-                  label="Available task(s)"
-                  v-model="jobSelection"
-                  :options="selectJob"
-                  :loading="!this.$store.state.isjobTableReady"
-                >
+                <va-select label="Available task(s)" v-model="jobSelection" :options="selectJob"
+                  :loading="!this.$store.state.isjobTableReady">
                   <template #prependInner>
-                    <va-icon
-                      name="work"
-                      size="small"
-                      color="primary"
-                    />
+                    <va-icon name="work" size="small" color="primary" />
                   </template>
                 </va-select>
               </va-card-content>
             </va-card>
           </template>
           <template #after>
-            <span
-              class="title title--info va-timeline-item__text"
-              :style="{color: 'info'}"
-            >
+            <span class="title title--info va-timeline-item__text" :style="{ color: 'info' }">
               Select type of task you want to kickstart
             </span>
           </template>
         </va-timeline-item>
-        <va-timeline-item v-if="isValid(jobSelection) && jobSelection.mode != 'mounted'" color="success" :active="isValid(targetSelection) ? true : false">
+        <va-timeline-item v-if="isValid(jobSelection) && jobSelection.mode != 'mounted'" color="success"
+          :active="isValid(targetSelection) ? true : false">
           <template #before>
-            <span
-              class="title title--info va-timeline-item__text"
-              :style="{color: 'info'}"
-            >
+            <span class="title title--info va-timeline-item__text" :style="{ color: 'info' }">
               Define your task target
             </span>
           </template>
           <template #after>
-            <va-card
-              stripe
-              stripe-color="info"
-              class="mb-0"
-            >
+            <va-card stripe stripe-color="info" class="mb-0">
               <va-card-content>
-                <va-select
-                  v-if="jobSelection.mode === 'single'"
-                  label="Target"
-                  v-model="targetSelection"
-                  :options="selectData"
-                  :loading="!this.$store.state.isvmTableReady"
-                  searchable
-                  highlight-matched-text
-                >
+                <va-select v-if="jobSelection.mode === 'single'" label="Target" v-model="targetSelection"
+                  :options="selectData" :loading="!this.$store.state.isvmTableReady" searchable highlight-matched-text>
                   <template #prependInner>
-                    <va-icon
-                      name="adjust"
-                      size="small"
-                      color="primary"
-                    />
+                    <va-icon name="adjust" size="small" color="primary" />
                   </template>
                 </va-select>
-                <va-select
-                  v-else
-                  label="Target"
-                  v-model="targetSelection"
-                  :options="selectData2"
-                  :loading="!this.$store.state.ispoolTableReady"
-                >
+                <va-select v-else label="Target" v-model="targetSelection" :options="selectData2"
+                  :loading="!this.$store.state.ispoolTableReady">
                   <template #prependInner>
-                    <va-icon
-                      name="work"
-                      size="small"
-                      color="primary"
-                    />
+                    <va-icon name="work" size="small" color="primary" />
                   </template>
                 </va-select>
               </va-card-content>
             </va-card>
           </template>
         </va-timeline-item>
-        <va-timeline-item v-else-if="isValid(jobSelection) && jobSelection.mode == 'mounted'" color="success" :active="isValid(jobSelection) ? true : false">
+        <va-timeline-item v-else-if="isValid(jobSelection) && jobSelection.mode == 'mounted'" color="success"
+          :active="isValid(jobSelection) ? true : false">
           <template #before>
-            <span
-              class="title title--info va-timeline-item__text"
-              :style="{color: 'info'}"
-            >
+            <span class="title title--info va-timeline-item__text" :style="{ color: 'info' }">
               Select virtual machine
-            </span>            
+            </span>
           </template>
           <template #after>
-            <va-card
-              stripe
-              stripe-color="info"
-            >
+            <va-card stripe stripe-color="info">
               <va-card-content>
-                <virtual-machine-selector
-                  v-model="virtualMachineSelection"
-                />
+                <virtual-machine-selector v-model="virtualMachineSelection" />
               </va-card-content>
             </va-card>
           </template>
         </va-timeline-item>
-        <va-timeline-item color="success" v-if="isValid(targetSelection) && jobSelection.type === 'restore'" :active="isValid(backupSelection) ? true : false">
+        <va-timeline-item color="success" v-if="isValid(targetSelection) && jobSelection.type === 'restore'"
+          :active="isValid(backupSelection) ? true : false">
           <template #before>
-            <va-card
-              stripe
-              stripe-color="info"
-            >
+            <va-card stripe stripe-color="info">
               <va-card-content>
-                <backup-selector
-                  v-if="isValid(targetSelection) && jobSelection.type === 'restore'"
-                  v-model="backupSelection"
-                  :virtualMachine="targetSelection.value" :job="jobSelection.mode"
-                />
+                <backup-selector v-if="isValid(targetSelection) && jobSelection.type === 'restore'"
+                  v-model="backupSelection" :virtualMachine="targetSelection.value" :job="jobSelection.mode" />
               </va-card-content>
             </va-card>
           </template>
           <template #after>
-            <span
-              class="title title--info va-timeline-item__text"
-              :style="{color: 'info'}"
-            >
+            <span class="title title--info va-timeline-item__text" :style="{ color: 'info' }">
               Select backup you want to restore
             </span>
           </template>
         </va-timeline-item>
-        <va-timeline-item color="success" v-if="isValid(virtualMachineSelection) && jobSelection.type === 'restore'" :active="isValid(backupSelection) ? true : false">
+        <va-timeline-item color="success" v-if="isValid(virtualMachineSelection) && jobSelection.type === 'restore'"
+          :active="isValid(backupSelection) ? true : false">
           <template #before>
-            <va-card
-              stripe
-              stripe-color="info"
-            >
+            <va-card stripe stripe-color="info">
               <va-card-content>
-                <backup-selector
-                  v-if="isValid(virtualMachineSelection) && jobSelection.type === 'restore'"
-                  v-model="backupSelection"
-                  :virtualMachine="virtualMachineSelection.value" :job="jobSelection.mode"
-                />
+                <backup-selector v-if="isValid(virtualMachineSelection) && jobSelection.type === 'restore'"
+                  v-model="backupSelection" :virtualMachine="virtualMachineSelection.value" :job="jobSelection.mode" />
               </va-card-content>
             </va-card>
           </template>
           <template #after>
-            <span
-              class="title title--info va-timeline-item__text"
-              :style="{color: 'info'}"
-            >
+            <span class="title title--info va-timeline-item__text" :style="{ color: 'info' }">
               Select backup you want to restore
             </span>
           </template>
         </va-timeline-item>
 
-        <va-timeline-item color="success" v-if="isValid(backupSelection) && jobSelection.mode === 'mounted'" :active="isValid(backupSelection) ? true : false">
+        <va-timeline-item color="success" v-if="isValid(backupSelection) && jobSelection.mode === 'mounted'"
+          :active="isValid(backupSelection) ? true : false">
           <template #before>
-            <span
-              class="title title--info va-timeline-item__text"
-              :style="{color: 'info'}"
-            >
+            <span class="title title--info va-timeline-item__text" :style="{ color: 'info' }">
               Select the storage where you want to restore
             </span>
           </template>
           <template #after>
-            <va-card
-              stripe
-              stripe-color="info"
-            >
+            <va-card stripe stripe-color="info">
               <va-card-content>
-                <storage-selector
-                  v-if="isValid(virtualMachineSelection) && jobSelection.type === 'restore'"
-                  v-model="storageSelection"
-                />
+                <storage-selector v-if="isValid(virtualMachineSelection) && jobSelection.type === 'restore'"
+                  v-model="storageSelection" />
               </va-card-content>
             </va-card>
           </template>
-          
+
         </va-timeline-item>
 
         <va-timeline-item>
@@ -191,9 +121,7 @@
             <div class="flex-center ma-3">
               <va-button
                 v-if="(isValid(targetSelection) && jobSelection.type === 'backup') || (isValid(backupSelection) && jobSelection.type === 'restore')"
-                icon="rocket_launch"
-                @click="startJob()"
-              >
+                icon="rocket_launch" @click="startJob()">
                 Start task
               </va-button>
             </div>
@@ -211,10 +139,15 @@ import StorageSelector from '@/components/virtualmachines/StorageSelector.vue'
 import axios from 'axios'
 import * as spinners from 'epic-spinners'
 
+const taskFromQuery = {
+  backup: 'Backup (VM)',
+  restore: 'Disk Restore & Replace (VM)'
+};
+
 export default {
   name: 'KickstartJob',
   components: { ...spinners, BackupSelector, VirtualMachineSelector, StorageSelector },
-  data () {
+  data() {
     return {
       jobList: [],
       jobSelection: {},
@@ -234,20 +167,10 @@ export default {
       this.backupSelection = {}
     }
   },
-  created() {
-
-    if(this.$route.query.task === 'backup'){
-    this.jobSelection.text = 'Backup (VM)';
-    this.jobSelection.value = 'Backup (VM)';
-    this.jobSelection.mode = 'single';
-    this.jobSelection.type = 'backup';
-    }else if(this.$route.query.task === 'restore'){
-    this.jobSelection.text = 'Disk Restore & Replace (VM)';
-    this.jobSelection.value = 'Disk Restore & Replace (VM)';
-    this.jobSelection.mode = 'single';
-    this.jobSelection.type = 'restore';
-    }
-
+  mounted() {
+    // Does not work on direct URL access maybe because this.$store.state.jobList is not ready yet.
+    const taskValue = taskFromQuery[this.$route.query.task];
+    this.jobSelection = this.selectJob.find(x => x.value == taskValue);
   },
   computed: {
     selectJob() {
@@ -279,7 +202,7 @@ export default {
         return true
       }
     },
-    startJob () {
+    startJob() {
       //const self  = this
       let route = null
       let args = {}
@@ -293,7 +216,7 @@ export default {
           url = `${this.$store.state.endpoint.api}${route}${this.targetSelection.value}`
         }
       } else if (this.jobSelection.type === 'restore') {
-        if(this.jobSelection.mode === 'mounted') {
+        if (this.jobSelection.mode === 'mounted') {
           route = "/api/v1/tasks/restorespecificpath"
           url = `${this.$store.state.endpoint.api}${route}`
           args = {
@@ -314,29 +237,27 @@ export default {
         }
 
       }
-      axios.post(url, args, { headers: {'Content-Type': 'application/json', 'Authorization': `Bearer ${this.$keycloak.token}`}})
-      .then(response => {
-        this.$store.dispatch("requestBackupTask", { token: this.$keycloak.token })
-        this.$store.dispatch("requestRestoreTask", { token: this.$keycloak.token })
-        if (this.jobSelection.type === 'restore') {
-          this.$router.push('/admin/tasks/restore');
-        } else {
-          this.$router.push('/admin/tasks/backup');
-        }
-        this.$vaToast.init(({ title: response.data.state, message: `Task has been successfully triggered`, color: 'success' }))
-      })
-      .catch(function (error) {
-        if (error.response) {
-          // The request was made and the server responded with a status code
-          // that falls out of the range of 2xx
-          this.$vaToast.init({ message: 'Task triggering has failed', title: 'Error', color: 'danger' })
-        }
-      })
+      axios.post(url, args, { headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${this.$keycloak.token}` } })
+        .then(response => {
+          this.$store.dispatch("requestBackupTask", { token: this.$keycloak.token })
+          this.$store.dispatch("requestRestoreTask", { token: this.$keycloak.token })
+          if (this.jobSelection.type === 'restore') {
+            this.$router.push('/admin/tasks/restore');
+          } else {
+            this.$router.push('/admin/tasks/backup');
+          }
+          this.$vaToast.init(({ title: response.data.state, message: `Task has been successfully triggered`, color: 'success' }))
+        })
+        .catch(function (error) {
+          if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            this.$vaToast.init({ message: 'Task triggering has failed', title: 'Error', color: 'danger' })
+          }
+        })
 
-    }    
+    }
   }
 }
 </script>
-<style scoped>
-
-</style>
+<style scoped></style>
