b2085abb152979dfc49e6d2fc5dc4945b97d1035
m-dhellin <m.dhellin@dimsi.fr>
2024-10-15 15:32:51 +0200

    Fixed pool form

--- src/ui/src/pages/admin/resources/pools/PoolForm.vue
+++ src/ui/src/pages/admin/resources/pools/PoolForm.vue
@@ -14,9 +14,9 @@
         <br />
         <va-select
           label="Select policy"
-          v-model="policySelection"
-          :options="selectData"
-          :rules="[(value) => isValid(value) || 'Field is required']"
+          v-model="selectedPolicy"
+          :options="policyOptions"
+          :rules="[(value) => value || 'Field is required']"
         >
           <template #prependInner>
             <va-icon name="storage" size="small" color="primary" />
@@ -25,9 +25,9 @@
       </va-form>
       <br />
       <va-select
-        label="Select Connector"
-        v-model="connectorSelection"
-        :options="selectConnectorData"
+        label="Select connector"
+        v-model="selectedConnector"
+        :options="connectorOptions"
       />
       <br />
       <va-button class="mb-3" @click="$refs.form.validate()">
@@ -51,96 +51,88 @@ export default {
       poolId: this.$route.params.id,
       formPool: {
         name: "",
-        policy_id: null,
-        connector_id: null,
       },
-      inputValue1: null,
-      policySelection: {},
-      connectorSelection: {},
+      selectedPolicy: null,
+      selectedConnector: null,
     };
   },
   computed: {
     statePool() {
       return this.$store.state.resources.poolList.find(
-        (item) => item.id == this.poolId
+        (e) => e.id == this.poolId
       );
     },
-    selectData() {
-      return this.$store.state.resources.policyList.map((x) => ({
-        text: x.name,
-        value: x.id,
+    policyOptions() {
+      return this.$store.state.resources.policyList.map((e) => ({
+        text: e.name,
+        value: e.id,
       }));
     },
-    selectConnectorData() {
-      return this.$store.state.resources.connectorList.map((x) => ({
-        text: x.name,
-        value: x.id,
+    connectorOptions() {
+      return this.$store.state.resources.connectorList.map((e) => ({
+        text: e.name,
+        value: e.id,
       }));
     },
   },
   watch: {
     statePool: function () {
-      this.formPool = { ...this.statePool };
-      this.updateConnector(this.formPool.connector_id);
+      this.propagateStatePool();
     },
-    selectData: function () {
-      if (this.statePool && this.formPool.policy_id !== null) {
-        this.updatePolicy(this.formPool.policy_id);
+    policyOptions: function () {
+      if (this.selectedPolicy) {
+        this.updatePolicy(this.selectedPolicy.value);
       }
     },
-    selectConnectorData: function () {
-      if (this.statePool && this.formPool.connector !== null) {
-        this.updateConnector(this.formPool.connector);
+    connectorOptions: function () {
+      if (this.selectedConnector) {
+        this.updateConnector(this.selectedConnector.value);
       }
     },
   },
   mounted() {
     if (this.statePool) {
-      this.formPool = { ...this.statePool };
-      this.updatePolicy(this.formPool.policy_id);
-      this.updateConnector(this.formPool.connector_id);
+      this.propagateStatePool();
     }
   },
   methods: {
-    updateConnector(id) {
-      this.connectorSelection = this.selectConnectorData.find(
-        (item) => item.value == id
-      );
-    },
-    isValid(value) {
-      if (Object.keys(value).length < 1) {
-        return false;
-      } else {
-        return true;
-      }
+    propagateStatePool() {
+      this.formPool = { ...this.statePool };
+      this.updatePolicy(this.statePool.policy_id);
+      this.updateConnector(this.statePool.connector_id);
     },
     updatePolicy(id) {
-      this.policySelection = this.selectData.find((item) => {
-        return item.value == id;
-      });
+      this.selectedPolicy = this.policyOptions.find((e) => e.value == id);
+    },
+    updateConnector(id) {
+      this.selectedConnector = this.connectorOptions.find((e) => e.value == id);
+    },
+    exportPool() {
+      const pool = JSON.parse(JSON.stringify(this.formPool));
+      pool.policy_id = this.selectedPolicy.value;
+      pool.connector_id = this.selectedConnector.value;
+      return pool;
     },
     updatePool() {
-      const pool = this.formPool;
-      pool.policy_id = this.policySelection.value;
-      pool.connector_id = this.connectorSelection.value;
       this.$store.dispatch("updatePool", {
         vm: this,
         token: this.$keycloak.token,
-        poolValues: pool,
+        poolValues: this.exportPool(),
       });
     },
     addPool() {
       self = this;
-      const pool = this.formPool;
-      pool.policy_id = this.policySelection.value;
-      pool.connector_id = this.connectorSelection.value;
       axios
-        .post(`${this.$store.state.endpoint.api}/api/v1/pools`, pool, {
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${this.$keycloak.token}`,
-          },
-        })
+        .post(
+          `${this.$store.state.endpoint.api}/api/v1/pools`,
+          this.exportPool(),
+          {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${this.$keycloak.token}`,
+            },
+          }
+        )
         .then((response) => {
           this.$store.dispatch("requestPool", { token: this.$keycloak.token });
           this.$router.push("/admin/resources/pools");
