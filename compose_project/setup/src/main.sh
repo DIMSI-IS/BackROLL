47a72025d0d83780368de81f1cd9cf41672b36bb
m-dhellin <m.dhellin@dimsi.fr>
2025-02-26 11:46:49 +0100

    Comments in setup

--- compose_project/setup/src/main.sh
+++ compose_project/setup/src/main.sh
@@ -2,6 +2,10 @@ echo "Starting BackROLL configuration by $BACKROLL_HOST_USER…"
 
 host_directory=compose_project
 
+# Note that here, it is too late to set variables in environnement files.
+# The values defined now will be read only at the next start.
+
+# SSH keys
 ssh_directory="$host_directory/core/ssh/@$BACKROLL_MODE"
 mkdir -p "$ssh_directory"
 cp "$host_directory/core/ssh/config" "$ssh_directory/"
