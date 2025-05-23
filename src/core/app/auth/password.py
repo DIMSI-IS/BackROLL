d6c8acb58d577a0dbf86d2097d2dd426228e015a
m-dhellin <m.dhellin@dimsi.fr>
2025-05-23 22:02:52 +0200

    WIP UI & core code

---
+++ src/core/app/auth/password.py
@@ -0,0 +1,10 @@
+def register(username: str, password: str):
+    pass
+
+
+def token(username: str, password: str) -> str:
+    pass
+
+
+def verify(token: str) -> bool:
+    pass
--- src/ui/src/pages/auth/Login.vue
+++ src/ui/src/pages/auth/Login.vue
@@ -14,5 +14,40 @@
     </va-card>
 </template>
 <script>
+import axios from "axios"
+import { defineComponent } from "vue";
 
+export default defineComponent({
+    name: "Login",
+    data() {
+        return {
+            password: ""
+        }
+    },
+    methods: {
+        async login() {
+            try {
+                const response = await axios.post(
+                    `${this.$store.state.endpoint.api}/api/v1/login`,
+                    {
+                        password: this.password
+                    },
+                    { headers: { 'Content-Type': 'application/json' } })
+                this.$keycloak.token = response.data.token
+                this.$vaToast.init({
+                    title: "Login",
+                    message: "You are logged in.",
+                    color: 'success',
+                })
+            } catch (error) {
+                console.error(error)
+                this.$vaToast.init({
+                    title: 'Login failed.',
+                    message: error?.response?.data?.detail ?? error,
+                    color: 'danger'
+                })
+            }
+        }
+    }
+})
 </script>
