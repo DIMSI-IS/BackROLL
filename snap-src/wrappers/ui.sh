8be603395defb2d409893f283345530549475fcb
Matthias <matthidel@gmail.com>
2025-06-11 20:50:58 +0200

    UI env should be ok – To test with Keycloak

--- snap-src/wrappers/ui.sh
+++ snap-src/wrappers/ui.sh
@@ -11,21 +11,13 @@ source "$SNAP/configuration/main.sh"
 src="$SNAP/ui"
 dst="$SNAP_DATA/usr/share/nginx/html"
 
-echo "----"
-ls "$src"
-echo "----"
-
 # TODO Fix entrypoint.sh an call it if production with Docker is still relevant.
 # Use layout to make nginx call work ?
 
 cp -r "$src"/* "$dst"/
 for path in "$dst"/js/*; do
-    envsubst '$BACKROLL_VERSION $API_ENDPOINT_URL $OPENID_ISSUER $OPENID_CLIENTID $OPENID_REALM' < "$path" > envsubst_output
+    envsubst '$BACKROLL_VERSION $API_ENDPOINT_URL $OPENID_ISSUER $OPENID_REALM $OPENID_CLIENT_UI_ID' < "$path" > envsubst_output
     mv envsubst_output "$path"
 done
-
-echo "----"
-ls "$dst"
-echo "----"
 
 "$SNAP/usr/sbin/nginx" -g "daemon off;"
