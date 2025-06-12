19d6578b81634acf2429242b52ecdddc9705f7b6
m-dhellin <m.dhellin@dimsi.fr>
2025-06-12 14:59:08 +0200

    WIP

--- src/core/app/auth/password.py
+++ src/core/app/auth/password.py
@@ -1,18 +1,33 @@
-from app import database
+from sqlmodel import Session, select
+from app.database import init_db_connection, User
+import bcrypt
+import jwt
+
 
 def register(username: str, password: str):
-    user = User(id = )
-    engine = database.init_db_connection()
+    hash = bcrypt.hashpw(password)
+
+    user = User(name=username, password_hash=hash)
+    engine = init_db_connection()
     with Session(engine) as session:
-        session.add(new_policy)
+        session.add(user)
         session.commit()
-        session.refresh(new_policy)
-        return new_policy
 
 
 def token(username: str, password: str) -> str:
-    pass
+    engine = init_db_connection()
+    with Session(engine) as session:
+        statement = select(User).where(User.name == username)
+        results = session.exec(statement)
+        user: User = results.one()
+
+        if bcrypt.checkpw(password, user.password_hash):
+            return jwt.encode({
+                username: username
+            })
 
 
 def verify(token: str) -> bool:
-    pass
+    decoded = jwt.decode(token)
+    
+
