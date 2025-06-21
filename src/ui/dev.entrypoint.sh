3e70899792ad90a2ab6afafd6846ebac2079d906
m-dhellin <m.dhellin@dimsi.fr>
2025-06-21 12:45:29 +0200

    More consistent environment variable names

--- src/ui/dev.entrypoint.sh
+++ src/ui/dev.entrypoint.sh
@@ -5,7 +5,7 @@ for name in BACKROLL_VERSION \
             API_ENDPOINT_URL \
             OPENID_ISSUER \
             OPENID_REALM \
-            OPENID_CLIENTID \
+            OPENID_CLIENT_UI_ID \
             ;
 do
   export VUE_APP_$name=${!name}
