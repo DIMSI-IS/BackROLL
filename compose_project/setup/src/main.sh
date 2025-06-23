4814a82cd18455f4ad6a7ba845eaa60a4da279da
Matthias <134587221+m-dhellin@users.noreply.github.com>
2025-06-23 17:13:42 +0200

    Auto ssh setup (#56)
    * Getting SSH keys at startup
    
    * SSH directory setup inside components

--- compose_project/setup/src/main.sh
+++ compose_project/setup/src/main.sh
@@ -5,17 +5,8 @@ host_directory=compose_project
 # Note that here, it is too late to set variables in environnement files.
 # The values defined now will be read only at the next start.
 
-# SSH keys
+# SSH directory
 ssh_directory="$host_directory/core/ssh/@$BACKROLL_MODE"
 mkdir -p "$ssh_directory"
-cp "$host_directory/core/ssh/config" "$ssh_directory/"
-for key_type in rsa ed25519; do
-  key_file="$ssh_directory/id_$key_type"
-  if ! test -f "$key_file"; then
-      echo "Generating $key_type ssh key…"
-      ssh-keygen -t $key_type -b 2048 -N "" -C "$BACKROLL_HOST_USER@$BACKROLL_HOSTNAME(backroll)" -f "$key_file" -q
-      echo "…done."
-  fi
-done
 
 echo "BackROLL configuration is done."
