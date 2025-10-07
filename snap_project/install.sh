32e8e083da0b1359b2c2d09c965287667f698128
Ubuntu <ubuntu@ml110g7>
2025-10-07 12:29:00 +0200

    Server-side proxy – Snap flavour

--- snap_project/install.sh
+++ snap_project/install.sh
@@ -1,7 +1,9 @@
+date
 cd .. &&\
 snapcraft clean &&\
 snapcraft pack &&\
 # Too brutal : data loss.
 # snap remove backroll &&\
 snap install --devmode backroll_0.1_amd64.snap &&\
-snap logs -n=100 backroll
+snap logs -n=32 backroll
+date
