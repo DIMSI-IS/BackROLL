5f27a21278f8929be10deaa901b116cb2ca4b215
Ubuntu <ubuntu@ml110g7>
2025-10-09 13:43:16 +0200

    Versioned snap name

--- snap_project/install.sh
+++ snap_project/install.sh
@@ -1,9 +1,26 @@
 date
-cd .. &&\
-snapcraft clean &&\
-snapcraft pack &&\
+
+cd ..
+root_path=$(pwd)
+project_path=snap_project
+output_path="$project_path/output"
+pack_path="$output_path/pack"
+
+snapcraft clean
+snapcraft pack --output "$pack_path"
+
+cd "$pack_path"
+raw_name=$(echo *.snap)
+cd "$root_path"
+full_name="${raw_name%.snap}_$(git describe --tags).snap"
+source_path="$pack_path/$raw_name"
+destination_path="$output_path/$full_name"
+
+mv "$source_path" "$destination_path"
+
 # Too brutal : data loss.
-# snap remove backroll &&\
-snap install --devmode backroll_0.1_amd64.snap &&\
+# snap remove backroll
+snap install --devmode "$destination_path"
 snap logs -n=32 backroll
+
 date
