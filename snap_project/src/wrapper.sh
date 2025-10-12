769eda2452821f167200fabbb337bdada85a0898
m-dhellin <134587221+m-dhellin@users.noreply.github.com>
2025-10-12 15:55:22 +0200

    Deduplicated ui command

--- snap_project/src/wrapper.sh
+++ snap_project/src/wrapper.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+# TODO Rename command.sh ?
+
 service="$1"
+shift 1
+arguments="$@"
 
 source "$SNAP/configuration/main.sh"
 export PATH="$SNAP/bin:$SNAP/usr/sbin:$PATH"
@@ -11,4 +15,4 @@ case "$service" in
         ;;
 esac
 
-bash "$SNAP/commands/$service.sh"
+bash "$SNAP/commands/$service.sh" $@
--- src/ui/entrypoint.sh
+++ src/ui/entrypoint.sh
@@ -1,12 +1,13 @@
-#!/bin/sh
+source_path="$1"
+destination_path="$2"
 
-echo "Replacing environment variables"
-for file in /usr/share/nginx/html/js/*; do
-  mv "$file" "$file.tmp"
-  # TODO Only works the first time or else the change are not saved before restarting.
-  envsubst '$BACKROLL_VERSION $OPENID_ISSUER $OPENID_CLIENT_UI_ID $OPENID_REALM $DEFAULT_USER_NAME' < "$file.tmp" > "$file"
-  rm "$file.tmp"
+echo src="$source_path"
+echo dst="$destination_path"
+
+cp -r "$source_path"/* "$destination_path"/
+for path in "$destination_path"/js/*; do
+    envsubst '$BACKROLL_VERSION $OPENID_ISSUER $OPENID_REALM $OPENID_CLIENT_UI_ID $DEFAULT_USER_NAME' < "$path" > envsubst_output
+    mv envsubst_output "$path"
 done
 
-echo "Starting from build"
 nginx -g 'daemon off;'
