2f1b6a72aa139040d194630d6e21ff032b584d90
Ubuntu <ubuntu@ml110g7>
2025-11-03 12:01:08 +0100

    Fixed early exit

--- snap_project/install.sh
+++ snap_project/install.sh
@@ -7,10 +7,10 @@ main() {
     configuration_path="$project_path/src/configuration"
     output_path="$project_path/output"
     pack_path="$output_path/pack"
-    
+
     bash src/env/get_read_only.sh > "$configuration_path/read_only_base.env" || return 1
     bash src/env/get_default.sh > "$configuration_path/default_base.env" || return 1
-exit
+
     snapcraft clean && snapcraft pack --output "$pack_path" || return 1
 
     cd "$pack_path"
@@ -25,7 +25,7 @@ exit
     # Too brutal : data loss.
     # snap remove backroll
     snap install --devmode "$destination_path" && snap logs -n=32 backroll || return 1
-    
+
     date
 }
 
